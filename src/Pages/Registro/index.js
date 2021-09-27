edd277fab9e262e8f639dc8730c96a0f8a9e8f57
antonio <jaia_@hotmail.com>
2021-09-27 17:15:23 -0600

    media query in register form

--- src/Pages/Registro/index.js
+++ src/Pages/Registro/index.js
@@ -99,9 +99,6 @@ const UserRegister = () => {
                     setShowMessage(false)                   
                       }, 4000 ) 
         }
-
-
-
         
     }
 
@@ -110,164 +107,34 @@ const UserRegister = () => {
       <Col xs='12' md='4' className='container-form'>
         <h1 className='p-titles mt-2'>Registro{business &&' de negocio'}</h1>
        <div className='form-div'>
-       <Form className='rounded border p-4'>
-            <FormGroup>
-                <Label>Nombre</Label>
-                <Input name="name" onChange={userDataHandler}/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Apellidos</Label>
-                <Input name="lastName" onChange={userDataHandler}/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Correo</Label>
-                <Input name="email" onChange={userDataHandler}/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Contraseña</Label>
-                <Input type='password' name="password" onChange={userDataHandler} />
-            </FormGroup>
-            <FormGroup>
-                <Label>Teléfono</Label>
-                <Input name="phoneNumber" onChange={userDataHandler}/>
-            </FormGroup>
-            <FormGroup>
-                <Label>Domicilio</Label>
-                <Input name="address" onChange={userDataHandler}/>
-            </FormGroup>
-      {
-          business &&
-          <>
-            <FormGroup>
-                <Label>Nombre de negocio</Label>
-                <Input name="businessName" onChange={userDataHandler}/>
-            </FormGroup>
-            <div className='business-schedules'>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Lun</Label>
-                        <Input name="day" type='checkbox' onChange={disableHandler } value='monday' />
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.monday} data-day='monday'/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.monday} data-day='monday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Mar</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='tuesday'/>
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.tuesday} data-day='tuesday'/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.tuesday} data-day='tuesday' />
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Mie</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='wednesday' />
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.wednesday} data-day='wednesday'/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.wednesday} data-day='wednesday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Jue</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='thursday' />
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.thursday} data-day='thursday' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.thursday} data-day='thursday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Vie</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='friday' />
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.friday} data-day='friday'/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.friday} data-day='friday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Sab</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='saturday'/>
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.saturday} data-day='saturday'/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.saturday} data-day='saturday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-                <div className='days'>
-                    <FormGroup check>
-                        <Label check>Dom</Label>
-                        <Input name="day" type='checkbox'onChange={disableHandler} value='sunday' />
-                    </FormGroup>
-                    <div className='open-close'>
-                        <FormGroup>
-                            <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.sunday} data-day='sunday' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.sunday} data-day='sunday'/>
-                        </FormGroup>
-                    </div> 
-                </div>
-               
-            </div>
-           
-            <FormGroup>
-                <Label >Clabe interbancaria</Label>
-                <Input name="bankAccount" onChange={userDataHandler}/>
-            </FormGroup>
-
-            <div className="d-flex flex-column">
-            <FormGroup check>
-                <Label check>
-                <Input type="checkbox" name="deliveryHome" onChange={checkDeliveryHandler} />{' '}
-                A domicilio
-                </Label>
-            </FormGroup>                
-            <FormGroup>
-                <Label check>
-                <Input type="checkbox" name="deliveryPickup" onChange={checkDeliveryHandler}/>{' '}
-                En tienda
-                </Label>
-            </FormGroup>
-
-            </div>    
-         </>
-        }
-            <div className='business-div'>
-                <FormGroup check>
+       <Form className='rounded border p-3'>
+           <div className='customer-schedules p-1'>
+                <FormGroup>
+                    <Label>Nombre</Label>
+                    <Input name="name" onChange={userDataHandler}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Apellidos</Label>
+                    <Input name="lastName" onChange={userDataHandler}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Correo</Label>
+                    <Input name="email" onChange={userDataHandler}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Contraseña</Label>
+                    <Input type='password' name="password" onChange={userDataHandler} />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Teléfono</Label>
+                    <Input name="phoneNumber" onChange={userDataHandler}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Domicilio</Label>
+                    <Input name="address" onChange={userDataHandler}/>
+                </FormGroup>
+                <div className='business-div'>
+                <FormGroup check className='p-2'>
                     <Label check>
                     <Input type="checkbox" name='rol' onClick={toggle} />{' '}
                     Registrarme como negocio
@@ -275,6 +142,140 @@ const UserRegister = () => {
                 </FormGroup>
                 <Button className='btn-p-primary' onClick={onSubmit}>Registrarse</Button>
             </div>
+            </div>
+
+      {
+          business &&
+        <>
+          <div className='business-schedules p-1'>
+                <FormGroup>
+                    <Label>Nombre de negocio</Label>
+                    <Input name="businessName" onChange={userDataHandler}/>
+                </FormGroup>
+                
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Lun</Label>
+                            <Input name="day" type='checkbox' onChange={disableHandler } value='monday' />
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.monday} data-day='monday'/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.monday} data-day='monday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Mar</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='tuesday'/>
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.tuesday} data-day='tuesday'/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.tuesday} data-day='tuesday' />
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Mie</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='wednesday' />
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.wednesday} data-day='wednesday'/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.wednesday} data-day='wednesday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Jue</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='thursday' />
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.thursday} data-day='thursday' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.thursday} data-day='thursday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Vie</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='friday' />
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.friday} data-day='friday'/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.friday} data-day='friday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Sab</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='saturday'/>
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.saturday} data-day='saturday'/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.saturday} data-day='saturday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+                    <div className='days'>
+                        <FormGroup check>
+                            <Label check>Dom</Label>
+                            <Input name="day" type='checkbox'onChange={disableHandler} value='sunday' />
+                        </FormGroup>
+                        <div className='open-close'>
+                            <FormGroup>
+                                <Input name="openTime" type='time' onChange={dayDataHandler} disabled={days.sunday} data-day='sunday' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input name="closeTime" type='time' onChange={dayDataHandler} disabled={days.sunday} data-day='sunday'/>
+                            </FormGroup>
+                        </div> 
+                    </div>
+
+                <FormGroup>
+                    <Label >Clabe interbancaria</Label>
+                    <Input name="bankAccount" onChange={userDataHandler}/>
+                </FormGroup>
+
+                <div className="delivery">
+                <FormGroup check>
+                    <Label check>
+                    <Input type="checkbox" name="deliveryHome" onChange={checkDeliveryHandler} />{' '}
+                    A domicilio
+                    </Label>
+                </FormGroup>                
+                <FormGroup>
+                    <Label check>
+                    <Input type="checkbox" name="deliveryPickup" onChange={checkDeliveryHandler}/>{' '}
+                    En tienda
+                    </Label>
+                </FormGroup>
+
+                </div> 
+           </div>   
+         </>
+        }
+           
             {   showMessage &&
                         <Alert color="danger" className="d-block mt-2 " >
                             Hubo un error al registrar usuario: { message }
