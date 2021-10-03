3c80607821105d82f22891e2434eee0ce14459a9
oscar-rh <oscar.rodriguezh@gmail.com>
2021-10-03 17:41:14 -0500

    se añade funcionalidad  para mostrar el detalle de pedido del negocio

--- src/Components/OrderStatus/index.js
+++ src/Components/OrderStatus/index.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import { Collapse, Button, CardBody, Card, CardText, 
+          CardTitle, CardSubtitle, Input , FormGroup, Label, Col} from 'reactstrap';
 import './styles.scss'
 
 const OrderDetail = (props) => {
+
+
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
+  const { client, products, total, parentOrder, createdAt, deliveryCost, _id }  = props.order    
+  const date = new Date(createdAt);
+  const resultDate = date.toLocaleDateString()
+
+  const onClickClose = (event) => {
+          console.log(event.target.dataset.order)
+  }
+   
+  
   return (
+ 
     <div className='order-div'>
         <div className='order-detail rounded border'
            onClick={toggle}>
             <div className='order-client-div'>
-                <h3 className='order'>Pedido #1</h3>
-               { /*<h4 className='client'>Cliente:</h4> */} 
+                <h3 className='order'>Pedido #{parentOrder}</h3>
+                <h2 className='client'>Cliente: { client.name }  { client.lastName } </h2>  
             </div>
             <div className='price-pending-div'>
-                <h5 className='price'>$35.00</h5>
+                <h5 className='price'>${total}</h5>
                 <div className='pending'>
                     Estado
                 </div>
@@ -25,7 +39,19 @@ const OrderDetail = (props) => {
       <Collapse isOpen={isOpen}>
         <Card>
           <CardBody>
-          Aqui se vera la informacion del pedido o del negocio
+              <CardTitle tag="h5">Fecha Orden {resultDate}</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">Costo de Entrega: ${deliveryCost}</CardSubtitle>
+              { products.map( product => {  
+                   return (<CardText tag="h6" className="mb-2 text-muted">  { product.qty } x { product.product.name }  ${ product.price } </CardText>)
+                } )                     
+              } 
+              <FormGroup row>
+                <Label for="confirmationCode" sm={2}>Código de Confirmación</Label>
+                <Col sm={2}>
+                  <Input name="confirmationCode" id="confirmationCode" placeholder="" />
+                </Col>
+              </FormGroup>
+              <Button data-order={_id} onClick={onClickClose} className="btn btn-p-primary" >Cerrar</Button>             
           </CardBody>
         </Card>
       </Collapse>
