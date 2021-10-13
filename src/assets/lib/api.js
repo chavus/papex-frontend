783af744b3eedcab32deba28d468967194e538bb
chavus <nojiso@gmail.com>
2021-10-13 11:46:57 -0500

    switched api base url

--- src/assets/lib/api.js
+++ src/assets/lib/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://papex-reflective-bat-wq.mybluemix.net"
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://papex-reflective-bat-wq.mybluemix.net"
+// const BASE_URL = "http://localhost:8080"
 
 
 export default{
